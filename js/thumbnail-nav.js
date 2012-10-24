668707142048773c942ba09a05bf14ac33e1ffa7
Erik Svedin <svedin.erik@gmail.com>
2012-10-24 17:52:48 +0200

    removed unneccesary files and improved the plugin to make it work with several navigations on the same site

--- js/thumbnail-nav.js
+++ js/thumbnail-nav.js
@@ -1,65 +1,77 @@
 (function( $ ){
 
-    $.fn.thumbnailNavigation = function() {
 
-    element = $(this.selector)
+    $.thumbnailNavigation = function(element) {
 
-    // Gets the list element and the total number of items in the list
-    carousel = $(element + '.navigation-outer-wrap .thumbnail-navigation-list');
-    firstItem = carousel.find('li:first-of-type');
-    totalItems = carousel.find('li').length;
+        var element = element;
 
-    // Calculates the itemwidth (assumes that all items are of euqal width)
-    // and calculates the total items visible in the outer-wrap.
-    itemWidth = firstItem.outerWidth() + 3;
-    wrap = element.find('.navigation-outer-wrap');
-    visibleItems = parseInt(wrap.width() / itemWidth);
+        $(element).children('.navigation.right').click(function(){
+            carousel = $(element).children('.navigation-outer-wrap').children('.thumbnail-navigation-list');
+            firstItem = carousel.find('li:first-of-type');
+            totalItems = carousel.find('li').length;
+            itemWidth = firstItem.outerWidth();
+            wrap = $(element).children('.navigation-outer-wrap');
+            visibleItems = parseInt(wrap.width() / itemWidth);
 
 
-    $(element + '.navigation.right').click(function(){
+            $current = carousel.find('li.active');
+            $current.removeClass('active');
 
-        $current = carousel.find('li.active');
-        $current.removeClass('active');
-
-        if(totalItems > visibleItems) {
-            if (($current.index()+1) >= (visibleItems - 1) && $current.index() !== totalItems){
-                offset = (($current.index()+2) - visibleItems)
-                carousel.css({'margin-left': '-'+ itemWidth * offset + 'px'});
+            if(totalItems > visibleItems) {
+                if (($current.index()+1) >= (visibleItems - 1) && $current.index() !== totalItems){
+                    offset = (($current.index()+2) - visibleItems)
+                    carousel.css({'margin-left': '-'+ itemWidth * offset + 'px'});
+                }
             }
-        }
 
-        if (($current.index() + 1) === totalItems){
-            carousel.css({'margin-left': '0'});
-        }
+            if (($current.index() + 1) === totalItems){
+                carousel.css({'margin-left': '0'});
+            }
 
-        $next = $current.next().length == 0 ? carousel.find('li').eq(0) : $current.next();
-        $next
-        .find('a[data-toggle="tab"]')
-        .click()
-    });
+            $next = $current.next().length == 0 ? carousel.find('li').eq(0) : $current.next();
+            $next
+            .find('a[data-toggle="tab"]')
+            .click()
+        });
 
+        $(element).children('.navigation.left').click(function() {
+            carousel = $(element).children('.navigation-outer-wrap').children('.thumbnail-navigation-list');
+            firstItem = carousel.find('li:first-of-type');
+            totalItems = carousel.find('li').length;
+            itemWidth = firstItem.outerWidth();
+            wrap = $(element).children('.navigation-outer-wrap');
+            visibleItems = parseInt(wrap.width() / itemWidth);
 
-    $(element + '.navigation.left').click(function() {
-        $current = carousel.find('li.active');
-        $current.removeClass('active');
+            $current = carousel.find('li.active');
+            $current.removeClass('active');
 
-        if($current.prev().length == 0 || $current.prev()[0].nodeName != 'LI') {
-            $prev = carousel.find('li').eq( carousel.find('li').length - 1)
-            offset = (($prev.index()+1) - visibleItems);
-            carousel.css({'margin-left': '-'+ itemWidth *offset+'px'});
-        }
-        else {
-            $prev =  $current.prev();
-            $offset = totalItems - ($prev.index()+1);
-            var margin = parseInt($(carousel).css("margin-left"));
-                if($offset >= visibleItems && margin < 0){carousel.css({'margin-left': '+=' + itemWidth});}
-                if(margin > (-itemWidth) && margin < 0){carousel.css({'margin-left': '0px'});}
-        }
+            if($current.prev().length == 0 || $current.prev()[0].nodeName != 'LI') {
+                $prev = carousel.children('li').eq( carousel.children('li').length - 1)
+                offset = (($prev.index()+1) - visibleItems);
+                carousel.css({'margin-left': '-'+ itemWidth *offset+'px'});
+            }
+            else {
+                $prev =  $current.prev();
+                $offset = totalItems - ($prev.index()+1);
+                var margin = parseInt($(carousel).css("margin-left"));
+                    if($offset >= visibleItems && margin < 0){carousel.css({'margin-left': '+=' + itemWidth});}
+                    if(margin > (-itemWidth) && margin < 0){carousel.css({'margin-left': '0px'});}
+            }
 
-        $prev
-        .find('a[data-toggle="tab"]')
-        .click()
-    });
-
+            $prev
+            .find('a[data-toggle="tab"]')
+            .click()
+        });
     }
+        $.fn.thumbnailNavigation = function() {
+            return this.each(function() {
+                if (undefined == $(this).data('thumbnailNavigation')) {
+                    var plugin = new $.thumbnailNavigation(this);
+                    $(this).data('thumbnailNavigation', plugin);
+                }
+            });
+
+        }
+
+
 })( jQuery );
